869a2ff98bf85e84eb164650960007dd06737e77
Tams Sokari <tams.sokari@andela.co>
2015-04-23 00:05:56 +0100

    set up routes for the api gateway

---
+++ app/routes/user-subs.route.js
@@ -0,0 +1,34 @@
+var userSubs = require('./../controllers/user-subs.controller');
+
+module.exports = function(router) {
+  router
+    .route('/signup')
+    .post(userSubs.signup);
+
+  router
+    .route('/login')
+    .post(userSubs.login);
+
+  router
+    .route('/:username/subscriptions')
+    .get(userSubs.getUserSubscriptions);
+
+  router
+    .route('/:username/subscriptions/posts')
+    .get(userSubs.getUserPostSubscriptions);
+  
+  router
+    .route('/:username/subscriptions/tags')
+    .get(userSubs.getUserTagSubscriptions);
+
+  router
+    .route('/:username/new-subs')
+    .post(userSubs.createNewSubscription);
+
+  router
+    .route('/:username/subscription/:_id')
+    .get(userSubs.getUserSubscription)
+    .put(userSubs.editUserSubscription)
+    .delete(userSubs.deleteUserSubscription);
+
+};
